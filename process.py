74f74ff6bb659df9fcbc7704b6de7d01ca3a24f4
Ruth Tillman <rtillman@nd.edu>
2016-10-25 09:58:43 -0400

    Added a very basic way to get and process dctitle

--- process.py
+++ process.py
@@ -1,2 +1,16 @@
 import csv, re
 from pymarc import MARCReader
+
+
+
+def getDCVariables():
+    with open("/Users/rtillman/Documents/Projects/CurateBooks/PyMARC/alephrecords.mrc", "rb") as marcfile:
+      reader = MARCReader(marcfile)
+      for record in reader:
+        dctitle = re.sub(' / ?', '', record.title())
+        dctitle = dctitle.rstrip('.')
+        print dctitle
+
+
+
+getDCVariables();
